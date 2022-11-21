a72e37e119bb9ec954b0cccc2f298ccd818e1e3c
Jona Gonzalez <jona.a.gonzalez104@gmail.com>
2022-11-21 14:03:39 -0600

    added console.log to userColor

--- js/external.js
+++ js/external.js
@@ -1,6 +1,7 @@
 "use strict";
 console.log("Hello from external JavaScript");
 alert('Welcome to my Website!');
-let userColor = prompt("What is your favorite color?");
+let userColor = prompt("What is your favorite color?")
+console.log(userColor);
 let message = "Great " + userColor + " is my favorite color too.";
 alert(message);
