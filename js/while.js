2ffa08697c5e10ee74314aa3d607886eadae1959
Jona Gonzalez <jona.a.gonzalez104@gmail.com>
2022-12-02 09:29:33 -0600

    conditionals exercise Done!

--- js/while.js
+++ js/while.js
@@ -1,7 +1,20 @@
-// i = 2
-// while(i <= 65536 ){
-//     console.log(`${i}`);
-//     i += i;
+let num = 2;
+while(num <= 65536 ){
+    console.log(num);
+    (num *= 2);
+}
+//
+// let numMultiplyBy = 2;
+// let counter = 0;
+// while(counter < 16) {
+//     console.log(numMultiplyBy);
+//     numMultiplyBy = numMultiplyBy * 2;
+//     counter++;
+// }
+
+// for (let i = 0, num = 2; i < 16; i++){
+//     console.log(num);
+//     num *= 2;
 // }
 
 var allCones = Math.floor(Math.random() * 50) + 50;
@@ -11,7 +24,7 @@ var i = 0;
     do {
         var soldCones = Math.floor(Math.random() * (5 - 1)) + 1;
         if(allCones === 0){
-            console.log("Yay! sold them all");
+            console.log("Yay! I sold them all");
             break;
         }else if(allCones >= soldCones){
             console.log("Just sold " + soldCones + " cones.");
