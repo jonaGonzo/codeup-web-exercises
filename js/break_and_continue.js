5f47b4670b95fb6ed238a57e51d7847b4dcd3411
Jona Gonzalez <jona.a.gonzalez104@gmail.com>
2022-12-04 16:02:35 -0600

    Arrays mini exercise and bonus complete.

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -1,12 +1,13 @@
 while(true){
     let userNum = prompt("Enter an odd number between 1 and 50:");
     userNum = parseFloat(userNum);
+    console.log(`Number to skip is: ${userNum}`)
     if (userNum % 2 === 1 && userNum <= 50 && userNum >= 1) {
         for (let i = 1; i <= 50; i+=2){
             if(i === userNum){
                 console.log(`Yikes! Skipping number ${i}`)
             }else{
-                console.log(i);
+                console.log(`Here is an odd number: ${i}`);
             }
         }
         break;
