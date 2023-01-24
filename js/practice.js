6fee508055fdd2b62731b2d4d54afc34912a871d
Jona Gonzalez <jona.a.gonzalez104@gmail.com>
2023-01-24 17:12:58 -0600

    weather map styled sexy and exercise complete

--- js/practice.js
+++ js/practice.js
@@ -447,4 +447,52 @@
 // console.log(encodeStr('SASS')) // returns ‘$@$$’
 // console.log(encodeStr('bike')) // returns ‘b1ke’
 
+// Make a function, reverseStrings, that takes in an array of objects and reverses the value of the 'str' properties.
 
+function reverseStrings(strs) {
+    for(let i=0; i<strs.length; i++){
+     strs[i].str = strs[i].str.split("").reverse().join("")
+    }
+    return strs
+}
+
+
+    const strs = [
+    {
+        id: 1,
+        str: 'hello'
+    },
+    {
+        id: 2,
+        str: 'world'
+    },
+    {
+        id: 3,
+        str: 'codeup'
+    },
+    {
+        id: 4,
+        str: 'x'
+    }
+]
+
+console.log(reverseStrings(strs)) // returns...
+    //
+    // [
+    // {
+    //     id: 1,
+    //     str: 'olleh'
+    // },
+    //     {
+    //         id: 2,
+    //         str: 'dlrow'
+    //     },
+    //     {
+    //         id: 3,
+    //         str: 'puedoc'
+    //     },
+    //     {
+    //         id: 4,
+    //         str: 'x'
+    //     }
+    // ]
