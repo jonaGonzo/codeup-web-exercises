c70a0109531d4ac9391e805e83f55dcbc9334b80
Jona Gonzalez <jona.a.gonzalez104@gmail.com>
2023-01-30 15:23:06 -0600

    promises exercise complete

--- js/promises.js
+++ js/promises.js
@@ -1,10 +1,30 @@
-async function publicEvents(){
-    try {
-            let response = await fetch("https://api.github.com/users/jonaGonzo/events/public", {headers: {'Authorization': `token ${keys.github}`}});
-            console.log(response);
-        return response.json()
-    }catch (error){
-            console.log(error)
-    }
-}
+(async () => {
+   const publicEvents = async (username) => {
+        try {
+            let response = await fetch(`https://api.github.com/users/${username}/events`, {headers: {'Authorization': `token ${keys.github}`}});
+            // console.log(response);
+            let data = await response.json();
+            // console.log(data)
+            return data
 
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    const timeCommit = async (username) => {
+        try {
+           let time = await publicEvents(username)
+            let lastCommit = time[0].created_at;
+            // console.log(lastCommit)
+            const date = new Date(`${lastCommit}`);
+           return `${username}'s last commit was on: ${date.toLocaleDateString()} @ ${date.toLocaleTimeString('en-US')}`
+        } catch (error) {
+        console.log(error)
+    }
+        console.log(timeCommit)
+    }
+
+    let answer = await timeCommit("jonaGonzo")
+    console.log(answer)
+})();
